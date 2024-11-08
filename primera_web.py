283bbaf71feeb8a0645398560910f74dd09a9f12
Tyrone2617 <by4on123@gmail.com>
2024-11-08 16:28:23 -0300

    Delete primera_web.py

--- primera_web.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st 
-
-  st.title("mi primera aplicación en streamlit")
-  st.header("este es mi encabezado")
-  st.subheader("este es un subencabezado")
-  st.write("esta es una introducción a streamlit, donde exploraremos como construir aplicaciones interactivas")
