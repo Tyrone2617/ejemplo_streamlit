00823e8adc4650fc4321f64ce577fdcc86be77a2
Tyrone2617 <by4on123@gmail.com>
2024-11-08 16:14:22 -0300

    Update primera_web.py

--- primera_web.py
+++ primera_web.py
@@ -1,9 +1,6 @@
 import streamlit as st 
 
   st.title("mi primera aplicación en streamlit")
-
   st.header("este es mi encabezado")
-  
   st.subheader("este es un subencabezado")
-
   st.write("esta es una introducción a streamlit")
