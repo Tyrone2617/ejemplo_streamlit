a28a79ef249c99fd1eb2417fd6c1cb20b94f6d50
Tyrone2617 <by4on123@gmail.com>
2024-11-08 16:22:08 -0300

    Update primera_web.py

--- primera_web.py
+++ primera_web.py
@@ -3,4 +3,4 @@ import streamlit as st
   st.title("mi primera aplicación en streamlit")
   st.header("este es mi encabezado")
   st.subheader("este es un subencabezado")
-  st.write("esta es una introducción a streamlit")
+  st.write("esta es una introducción a streamlit, donde exploraremos como construir aplicaciones interactivas")
